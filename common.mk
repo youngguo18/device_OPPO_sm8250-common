7e9fb10b0c31fb0ec0fa150b7dcfa481d97da68a
aamirr-ali <amirwide@gmail.com>
2023-12-27 01:42:57 +0530

    sm8350-common: Enable freeform windows

--- common.mk
+++ common.mk
@@ -500,6 +500,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # VNDK
 PRODUCT_PACKAGES += \
     vndservicemanager
