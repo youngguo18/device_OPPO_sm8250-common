d8dd613274ffff1e1d3de5fa5cfab9ce216b909f
Bruno Martins <bgcngm@gmail.com>
2024-07-15 17:15:27 +0530

    sm8250-common: Convert WiFi firmware symlinks to install_symlink targets [2/2]
    The last bit that was preventing building hermetic images.
    
    This should fix the missing symlinks and allow wifi to work.

--- common.mk
+++ common.mk
@@ -574,7 +574,9 @@ PRODUCT_BOOT_JARS += \
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
-    firmware_WCNSS_qcom_cfg.ini_symlink
+    firmware_WCNSS_qcom_cfg.ini_symlink \
+    firmware_WCNSS_qcom_cfg.ini_qca6390_symlink \
+    firmware_wlanmdsp.otaupdate_symlink
 
 # Testing to correct brightness
 PRODUCT_SYSTEM_PROPERTIES += \
