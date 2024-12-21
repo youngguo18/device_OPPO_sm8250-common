697d1b5e1580d5ce5c5c3351b7c638e82bc77897
youngguo18 <guoamao@163.com>
2024-12-21 08:42:48 +0800

    sm8250-common: derp15 bringup

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -106,7 +106,7 @@ TARGET_USES_CUSTOM_C2_MANIFEST := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/device_framework_matrix.xml \
     $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/derp/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += \
     $(COMMON_PATH)/manifest.xml \
